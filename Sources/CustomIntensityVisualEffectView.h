13a8185c9135ac89c0a41c653eccac6f8c1736c8
Dariusz Rybicki <darrarski@gmail.com>
2017-11-27 23:34:20 +0100

    Add Cocoa Touch Framework target

---
+++ Sources/CustomIntensityVisualEffectView.h
@@ -0,0 +1,4 @@
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double CustomIntensityVisualEffectViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char CustomIntensityVisualEffectViewVersionString[];
