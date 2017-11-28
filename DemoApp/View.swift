413e3219b0681d1589d6057479fe79cba250bd99
Dariusz Rybicki <darrarski@gmail.com>
2017-11-28 18:51:14 +0100

    Add labels and refactor layout in View class

--- DemoApp/View.swift
+++ DemoApp/View.swift
@@ -20,20 +20,28 @@ class View: UIView {
     let imageView = Subview.imageView
     let textLabel = Subview.textLabel
     let visualEffectView = Subview.visualEffectView
-    let slider = Subview.slider
+    let configView = Subview.configView
+    let contentLabel = Subview.configLabel
     let contentSegmentedControl = UISegmentedControl(items: [])
+    let effectLabel = Subview.configLabel
     let firstEffectSegmentedControl = UISegmentedControl(items: [])
     let secondEffectSegmentedControl = UISegmentedControl(items: [])
+    let sliderLabel = Subview.configLabel
+    let slider = Subview.slider
 
     private func addSubviews() {
         addSubview(contentView)
         contentView.addSubview(textLabel)
         contentView.addSubview(imageView)
         contentView.addSubview(visualEffectView)
-        addSubview(contentSegmentedControl)
-        addSubview(firstEffectSegmentedControl)
-        addSubview(secondEffectSegmentedControl)
-        addSubview(slider)
+        addSubview(configView)
+        configView.addSubview(contentLabel)
+        configView.addSubview(contentSegmentedControl)
+        configView.addSubview(effectLabel)
+        configView.addSubview(firstEffectSegmentedControl)
+        configView.addSubview(secondEffectSegmentedControl)
+        configView.addSubview(sliderLabel)
+        configView.addSubview(slider)
     }
 
     // MARK: Layout
@@ -43,18 +51,19 @@ class View: UIView {
         contentView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         contentView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         contentView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
 
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 0.67).isActive = true
-        imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 32).isActive = true
+        imageView.topAnchor.constraintEqualToSystemSpacingBelow(contentView.topAnchor, multiplier: 4).isActive = true
         imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
         imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
 
         textLabel.translatesAutoresizingMaskIntoConstraints = false
-        textLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16).isActive = true
-        textLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16).isActive = true
-        textLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
-        textLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -16).isActive = true
+        textLabel.topAnchor.constraintEqualToSystemSpacingBelow(imageView.bottomAnchor, multiplier: 2).isActive = true
+        textLabel.leftAnchor.constraintEqualToSystemSpacingAfter(contentView.leftAnchor, multiplier: 2).isActive = true
+        contentView.rightAnchor.constraintGreaterThanOrEqualToSystemSpacingAfter(textLabel.rightAnchor, multiplier: 2).isActive = true
+        contentView.bottomAnchor.constraintGreaterThanOrEqualToSystemSpacingBelow(textLabel.bottomAnchor, multiplier: 2).isActive = true
         textLabel.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
 
         visualEffectView.translatesAutoresizingMaskIntoConstraints = false
@@ -63,32 +72,67 @@ class View: UIView {
         visualEffectView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
         visualEffectView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
 
+        configView.translatesAutoresizingMaskIntoConstraints = false
+        configView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        configView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        configView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.topAnchor.constraintEqualToSystemSpacingBelow(configView.topAnchor, multiplier: 2).isActive = true
+        contentLabel.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintGreaterThanOrEqualToSystemSpacingAfter(contentLabel.rightAnchor, multiplier: 2).isActive = true
+
         contentSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        contentSegmentedControl.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 32).isActive = true
-        contentSegmentedControl.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
-        contentSegmentedControl.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
+        contentSegmentedControl.topAnchor.constraintEqualToSystemSpacingBelow(contentLabel.bottomAnchor, multiplier: 1).isActive = true
+        contentSegmentedControl.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintEqualToSystemSpacingAfter(contentSegmentedControl.rightAnchor, multiplier: 2).isActive = true
+
+        effectLabel.translatesAutoresizingMaskIntoConstraints = false
+        effectLabel.topAnchor.constraintEqualToSystemSpacingBelow(contentSegmentedControl.bottomAnchor, multiplier: 2).isActive = true
+        effectLabel.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintGreaterThanOrEqualToSystemSpacingAfter(effectLabel.rightAnchor, multiplier: 2).isActive = true
 
         firstEffectSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        firstEffectSegmentedControl.topAnchor.constraint(equalTo: contentSegmentedControl.bottomAnchor, constant: 16).isActive = true
-        firstEffectSegmentedControl.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
-        firstEffectSegmentedControl.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
+        firstEffectSegmentedControl.topAnchor.constraintEqualToSystemSpacingBelow(effectLabel.bottomAnchor, multiplier: 1).isActive = true
+        firstEffectSegmentedControl.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintEqualToSystemSpacingAfter(firstEffectSegmentedControl.rightAnchor, multiplier: 2).isActive = true
 
         secondEffectSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        secondEffectSegmentedControl.topAnchor.constraint(equalTo: firstEffectSegmentedControl.bottomAnchor, constant: 0).isActive = true
-        secondEffectSegmentedControl.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
-        secondEffectSegmentedControl.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
+        secondEffectSegmentedControl.topAnchor.constraintEqualToSystemSpacingBelow(firstEffectSegmentedControl.bottomAnchor, multiplier: 1).isActive = true
+        secondEffectSegmentedControl.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintEqualToSystemSpacingAfter(secondEffectSegmentedControl.rightAnchor, multiplier: 2).isActive = true
+
+        sliderLabel.translatesAutoresizingMaskIntoConstraints = false
+        sliderLabel.topAnchor.constraintEqualToSystemSpacingBelow(secondEffectSegmentedControl.bottomAnchor, multiplier: 2).isActive = true
+        sliderLabel.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintGreaterThanOrEqualToSystemSpacingAfter(sliderLabel.rightAnchor, multiplier: 2).isActive = true
 
         slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.topAnchor.constraint(equalTo: secondEffectSegmentedControl.bottomAnchor, constant: 16).isActive = true
-        slider.leftAnchor.constraint(equalTo: leftAnchor, constant: 32).isActive = true
-        slider.rightAnchor.constraint(equalTo: rightAnchor, constant: -32).isActive = true
-        slider.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32).isActive = true
+        slider.topAnchor.constraintEqualToSystemSpacingBelow(sliderLabel.bottomAnchor, multiplier: 1).isActive = true
+        slider.leftAnchor.constraintEqualToSystemSpacingAfter(configView.leftAnchor, multiplier: 2).isActive = true
+        configView.rightAnchor.constraintEqualToSystemSpacingAfter(slider.rightAnchor, multiplier: 2).isActive = true
+        configView.bottomAnchor.constraintEqualToSystemSpacingBelow(slider.bottomAnchor, multiplier: 4).isActive = true
     }
 
 }
 
 private extension View {
     struct Subview {
+        static var configView: UIView {
+            let view = UIView(frame: .zero)
+            view.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+            view.layer.borderColor = view.tintColor.cgColor
+            view.layer.borderWidth = 1
+            return view
+        }
+
+        static var configLabel: UILabel {
+            let label = UILabel(frame: .zero)
+            label.font = UIFont.preferredFont(forTextStyle: .caption1)
+            label.textColor = label.tintColor
+            return label
+        }
+
         static var textLabel: UILabel {
             let label = UILabel(frame: .zero)
             label.numberOfLines = 0
